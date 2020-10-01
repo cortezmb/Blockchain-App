b7de7318c6c289a3a43b9311867dadc5fa2b98a5
cortezmb <michael.cortez78@gmail.com>
2020-10-01 18:06:55 -0500

    set up react

---
+++ blockchain-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css';
+import Web3 from 'web3';
+
+class App extends Component {
+
+async loadBlockchainData() {
+
+  
+}
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1> Blockchain App</h1>
+        </div>
+      </>
+    )
+  }
+}
+
+export default App
